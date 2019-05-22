7ecc2e7b7cbe4fe800c9de7656ff3a3b40c92781
Samseppiol <j_hage@live.com>
2019-05-23 09:50:08 +1000

    Update exports, add travis and tests

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hl-component-ecs-cluster
+
+
+Must specify an ecs.config.yaml with a cluster_name parameter
--- ecs.cfndsl.rb
+++ ecs.cfndsl.rb
@@ -1,16 +1,17 @@
 CloudFormation do 
 
     ECS_Cluster('EcsCluster') {
-        ClusterName FnSub("${EnvironmentName}-#{cluster_name}") if defined? cluster_name
+        ClusterName FnSub(cluster_name) if defined? cluster_name
     }
 
+    export_name = defined?(cluster_name) ? cluster_name : "${EnvironmentName}-#{component_name}"
     Output("EcsCluster") {
-        Value(Ref('EcsCluster'))
-        Export FnSub("${EnvironmentName}-#{component_name}-EcsCluster")
-      }
+            Value(Ref('EcsCluster'))
+            Export FnSub("#{export_name}-EcsCluster")
+          }
     Output("EcsClusterArn") {
-        Value(FnGetAtt('EcsCluster','Arn'))
-        Export FnSub("${EnvironmentName}-#{component_name}-EcsClusterArn")
-    }
+            Value(FnGetAtt('EcsCluster','Arn'))
+            Export FnSub("#{export_name}-EcsClusterArn")
+        }
 
 end
